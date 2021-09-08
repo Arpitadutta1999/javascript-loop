387691399294047819fff945637e623ba5134a8e
Arpitadutta1999 <90311335+Arpitadutta1999@users.noreply.github.com>
2021-09-08 15:36:19 +0530

    Add files via upload

---
+++ for-of.js
@@ -0,0 +1,10 @@
+let criketers=["virat kholi", "MS Dhoni", "Sachin tendulkar", "Sourav Ganguly" ,"Yuvraj Singh"];
+for (let name of cricketer) {
+    console.log(name);
+
+}
+console.log("when we used for in**************");
+for (let name in cricketers){
+    console.log(name);
+
+}
