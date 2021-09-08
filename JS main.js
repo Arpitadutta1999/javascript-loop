d4b9e075c63aba7a3dd54a275c6ff919a922a9c6
Arpitadutta1999 <90311335+Arpitadutta1999@users.noreply.github.com>
2021-09-08 15:11:30 +0530

    Add files via upload

---
+++ JS main.js
@@ -0,0 +1,16 @@
+let obj={
+    name:"riya";
+    City:"kolkata";
+    address:"sindhu road";
+    language:JS;
+    district:"nadia";
+}
+for(let key in obj){
+    console.log(obj[key]);
+}
+let mystring="this is my first created string";
+for(let char in mystring) {
+    console.log(myString[char]);
+    
+
+}
